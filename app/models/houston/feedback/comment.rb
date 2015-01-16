e2574c987cc34f8741c3e8f5fda7e7b76387d0dc
Bob Lail <bob.lail@cph.org>
2015-01-16 14:03:35 -0600

    [improvement] Allowed matching only read or unread records (5m)

--- app/models/houston/feedback/comment.rb
+++ app/models/houston/feedback/comment.rb
@@ -29,7 +29,9 @@ module Houston
         def search(query_string)
           tags = []
           not_tags = []
+          flags = []
           query_string = query_string
+            .gsub(/\/(read|unread)/) { |arg| flags << $1; "" }
             .gsub(/\-\#([a-z\-0-9]+)/) { |arg| not_tags << $1; "" }
             .gsub(/\#([a-z\-0-9]+)/) { |arg| tags << $1; "" }
             .strip
@@ -53,6 +55,8 @@ module Houston
             results.where(["tags ~ ?", "(?n)^#{tag}$"]) } # (?n) specified the newline-sensitive option
           results = not_tags.inject(results) { |results, tag|
             results.where(["tags !~ ?", "(?n)^#{tag}$"]) } # (?n) specified the newline-sensitive option
+          results = results.where("flags.read IS TRUE") if flags.member? "read"
+          results = results.where("flags.read IS FALSE OR flags.read IS NULL") if flags.member? "unread"
           results = results.where(query.conditions)
             .select("feedback_comments.*", excerpt.as("excerpt"), rank.as("rank"))
             .order("rank DESC") unless query_string.blank?
