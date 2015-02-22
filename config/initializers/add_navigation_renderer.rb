ece1805d32868425dd76ac1fff33163cfcb99d5f
Bob Lail <bob.lail@cph.org>
2015-02-22 16:59:06 -0600

    [refactor] Refactored Navigation Renderer (1m)

--- config/initializers/add_navigation_renderer.rb
+++ config/initializers/add_navigation_renderer.rb
@@ -1,12 +1,5 @@
-Houston.config.add_navigation_renderer :feedback do
-  projects = followed_projects.select { |project| can?(:read, project) }
-  unless projects.empty?
-    menu_items = []
-    menu_items << MenuItem.new("All", Houston::Feedback::Engine.routes.url_helpers.all_feedback_path)
-    menu_items << MenuItemDivider.new
-    menu_items.concat projects.map { |project| ProjectMenuItem.new(project, Houston::Feedback::Engine.routes.url_helpers.project_feedback_path(project)) }
-    menu_items
-    
-    render_nav_menu "Feedback", icon: "fa-comment", items: menu_items
-  end
+Houston.config.add_project_feature :feedback do
+  name "Feedback"
+  icon "fa-comment"
+  path { |project| Houston::Feedback::Engine.routes.url_helpers.project_feedback_path(project) }
 end
