1ba07a2f0eafc177be82f80b4530275eabede8b5
Bob Lail <bob.lail@cph.org>
2015-10-10 11:30:52 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Houston::Feedback::Engine.routes.draw do
 
-  get "by_project/:slug", to: "project_feedback#index", as: :project_feedback
-  post "by_project/:slug", to: "project_feedback#create"
-  post "by_project/:slug/csv", to: "project_feedback#upload_csv", as: :upload_project_feedback
-  post "by_project/:slug/import", to: "project_feedback#import"
-  post "by_project/:slug/from_email", to: "project_feedback#from_email"
-  get "by_project/:slug/history", to: "project_feedback#history"
+  scope "feedback" do
+    get "by_project/:slug", to: "project_feedback#index", as: :project_feedback
+    post "by_project/:slug", to: "project_feedback#create"
+    post "by_project/:slug/csv", to: "project_feedback#upload_csv", as: :upload_project_feedback
+    post "by_project/:slug/import", to: "project_feedback#import"
+    post "by_project/:slug/from_email", to: "project_feedback#from_email"
+    get "by_project/:slug/history", to: "project_feedback#history"
 
-  delete "comments", to: "comments#destroy"
+    delete "comments", to: "comments#destroy"
 
-  post "comments/:id/read", to: "comments#mark_read"
-  post "comments/:id/unread", to: "comments#mark_unread"
-  put "comments/:id", to: "comments#update"
+    post "comments/:id/read", to: "comments#mark_read"
+    post "comments/:id/unread", to: "comments#mark_unread"
+    put "comments/:id", to: "comments#update"
 
-  delete "comments/tags", to: "tags#remove"
-  post "comments/tags", to: "tags#add"
+    delete "comments/tags", to: "tags#remove"
+    post "comments/tags", to: "tags#add"
+  end
 
 end
