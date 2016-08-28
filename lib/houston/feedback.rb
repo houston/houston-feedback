cecd9983726b5e3bbe503b369dc98fa09c4ef9a8
Bob Lail <bob.lailfamily@gmail.com>
2016-08-28 16:24:59 -0500

    [skip] Removed the deprecated comment option (1m)

--- lib/houston/feedback.rb
+++ lib/houston/feedback.rb
@@ -24,7 +24,6 @@ module Houston
 
   add_project_feature :feedback do
     name "Feedback"
-    icon "fa-comment"
     path { |project| Houston::Feedback::Engine.routes.url_helpers.project_feedback_path(project) }
   end
 
