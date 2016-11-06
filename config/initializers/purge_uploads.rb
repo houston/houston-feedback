7b040d336f932d50fd033afcccd7bd5d73898919
Bob Lail <bob.lailfamily@gmail.com>
2016-11-06 12:50:27 -0600

    [skip] Deprecated “At” triggers (1m)

--- config/initializers/purge_uploads.rb
+++ config/initializers/purge_uploads.rb
@@ -1,4 +1,4 @@
-Houston.config.at "10:00pm", "purge:uploads" do
+Houston.config.every "day at 10:00pm", "purge:uploads" do
   uploads_dir = File.join(Houston::Application.paths["tmp"].first, "uploads")
   earliest_upload_to_keep = "#{uploads_dir}/#{Time.now.beginning_of_day.strftime("%Y%m%d%H%M%S")}.csv"
   Dir.glob("#{uploads_dir}/*.csv").each do |file|
