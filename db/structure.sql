d823021ab02456620cd13ab04b366c04516df1d2
Bob Lail <bob.lailfamily@gmail.com>
2017-03-29 11:28:00 -0500

    [skip] Used houston-core 0.8.3, not the development version (1m)

--- db/structure.sql
+++ db/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.0
--- Dumped by pg_dump version 9.6.0
+-- Dumped from database version 9.6.2
+-- Dumped by pg_dump version 9.6.2
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -2565,7 +2565,7 @@ ALTER TABLE ONLY feedback_snippets
 
 SET search_path TO "$user", public;
 
-INSERT INTO schema_migrations (version) VALUES
+INSERT INTO "schema_migrations" (version) VALUES
 ('20120324185914'),
 ('20120324202224'),
 ('20120324212848'),
